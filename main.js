b87fbd471474b7a9ff0d52402d284bef30291076
Camilo Buitrago <cbuitrago@wearenovae.com>
2022-08-18 16:52:14 -0500

    Nav mobile, cambio toggle

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const navMail = document.querySelector('.navbar-email');
 const desktopNav = document.querySelector('.desktop-menu');
+const navHamIcon = document.querySelector('.menu');
+const mobileNav = document.querySelector('.mobile-menu');
+
 
 navMail.addEventListener('click', toggleNav);
 
-function toggleNav() {
-  desktopNav.classList.remove('inactive');
+function toggleNav(){
+  desktopNav.classList.toggle('inactive');
+}
+
+navHamIcon.addEventListener('click', toggleNavMobile);
+
+function toggleNavMobile() {
+  mobileNav.classList.toggle('inactive');
 }
