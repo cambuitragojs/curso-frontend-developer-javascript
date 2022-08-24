6d3b4fed0f66a7c2286a460df687e573b903298e
Camilo Buitrago <cbuitrago@wearenovae.com>
2022-08-24 16:13:55 -0500

    menús desplegables del header

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
+// Variables
 const navMail = document.querySelector('.navbar-email');
 const desktopNav = document.querySelector('.desktop-menu');
 const navHamIcon = document.querySelector('.menu');
 const mobileNav = document.querySelector('.mobile-menu');
+const iconCart = document.querySelector('.navbar-shopping-cart')
+const asideCart = document.querySelector('.product-detail')
 
-
+// Menú desktop
 navMail.addEventListener('click', toggleNav);
 
 function toggleNav(){
+  if (!asideCart.classList.contains('inactive')) {
+    asideCart.classList.add('inactive');
+  }
   desktopNav.classList.toggle('inactive');
+  navMail.classList.toggle('active');
 }
 
+// Menú mobile
 navHamIcon.addEventListener('click', toggleNavMobile);
 
 function toggleNavMobile() {
+  if (!asideCart.classList.contains('inactive')){
+    asideCart.classList.add('inactive');
+  }
   mobileNav.classList.toggle('inactive');
+}
+
+// Menú Cart
+iconCart.addEventListener('click', toggleIconCart);
+
+function toggleIconCart() {
+  if (!mobileNav.classList.contains('inactive') || !desktopNav.classList.contains('inactive')) {
+    mobileNav.classList.add('inactive');
+    desktopNav.classList.add('inactive');
+  }
+  asideCart.classList.toggle('inactive');
 }
